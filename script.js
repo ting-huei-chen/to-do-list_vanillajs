e4c9fcfed3509f2582fadf0a178aeab15f1cab32
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-07-31 00:45:55 -0600

    2022-07-31 update

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-var addbtn = document.getElementById("addbtn");
 var field = document.querySelector("main");
-var clearbtn = document.getElementById("clearbtn");
-var sticker = document.getElementById("sticker");
-var newNote = document.getElementById("newNote");
 loadEvents();
 
-function createElem(val) {
+function createElem(e,val) {
     let elem = document.createElement("li");
     let del = document.createElement("input");
     del.type = "checkbox";
     let intext = document.createElement("p");
     elem.classList.add("item");
     del.classList.add("del");
-    sticker.appendChild(elem);
+    // sticker.appendChild(elem);
+    e.parentNode.previousElementSibling.appendChild(elem);
     intext.innerHTML = val;
     elem.appendChild(intext);
     elem.appendChild(del);
 }
 
 // Enter key access
-document.getElementById("todo").addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-    addbtn.click();
-    }
-});
+var inputBars = document.querySelectorAll(".inputBar");
+inputBars.forEach((e)=>{
+    e.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            e.nextElementSibling.click();
+        }
+    });
+})
 
 function tick(e){
     let item = e.target.previousElementSibling;
@@ -39,15 +39,20 @@ function clearDone() {
 
 function loadEvents(){
     var newList = document.querySelectorAll(".del");
+    var addbtn = document.querySelectorAll(".addbtn");
+    var clearbtn = document.querySelectorAll(".clearbtn");
     newList.forEach((e)=>e.addEventListener("click", tick));
-    clearbtn.addEventListener("click", clearDone);
-    addbtn.addEventListener("click", function () {
-        let todo = document.getElementById("todo");
-        createElem(todo.value);
-        var newList = document.querySelectorAll(".del");
-        newList.forEach((e)=>e.addEventListener("click",tick));
-        todo.value = "";
-    });
+    clearbtn.forEach((e)=>e.addEventListener("click", clearDone));
+    addbtn.forEach((e)=>{
+        e.addEventListener("click", function () {
+            // let todo = document.getElementById("todo");
+            createElem(e,e.previousElementSibling.value);
+            // createElem(e,todo.value);
+            var newList = document.querySelectorAll(".del");
+            newList.forEach((e)=>e.addEventListener("click",tick));
+            todo.value = "";
+        });
+    })
 }
 
 function createNote(){
@@ -60,7 +65,9 @@ function createNote(){
     let inputText = document.createElement("input");
     let inputSubmit = document.createElement("input");
     inputText.type="text";
+    inputText.classList.add("inputBar");
     inputSubmit.type="submit";
+    inputSubmit.classList.add("addbtn");
     let clearbtn = document.createElement("button");
     clearbtn.innerHTML="Clear Done";
     inputSubmit.value="+";
@@ -69,8 +76,10 @@ function createNote(){
     addSec.append(inputText,inputSubmit,clearbtn)
     note.append(elemList,addSec);
     field.appendChild(note);
+    inputBars = document.querySelectorAll(".inputBar");
 }
 
 newNote.addEventListener("click",()=>{
     createNote();
+    loadEvents();
 })
